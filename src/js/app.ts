460c7e2660ab39a6ae3ae4836d2d470430941188
Thomas Dekiere <t.dekiere@gmail.com>
2018-05-13 14:53:23 +0200

    show realtime subtitles from google cloud firestore with translations

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,1 +1,98 @@
 /// <reference path="lib.ts"/>
+/// <reference path="../config.ts"/>
+
+declare var firebase: any;
+
+namespace thdk.rst {
+    export class RemoteSubtitleReceiver {
+        private db: any;
+        private $container: JQuery;
+        private dbSubtitleRef: any;
+
+        private translateService: translate.ITranslateService;
+
+        constructor(config: IAppConfig, translateService: translate.ITranslateService) {
+            firebase.initializeApp({
+                apiKey: config.apiKey,
+                authDomain: 'czech-subs-1520975638509.firebaseapp.com',
+                projectId: 'czech-subs-1520975638509'
+            });
+
+            this.translateService = translateService;
+
+            // Initialize Cloud Firestore through Firebase
+            this.db = firebase.firestore();
+            const settings = {/* your settings... */ timestampsInSnapshots: true };
+            this.db.settings(settings);
+
+            this.dbSubtitleRef = this.db.collection("subtitles");
+
+            this.$container = $("#subsContainer");
+        }
+
+        public init() {
+
+            this.$container.on("click", ".sub", (e) => {
+                const $target = $(e.currentTarget);
+                const subId = $target.attr("data-subid");
+                this.translateService.translate($target.find("p.original").html()).then(translation => {
+                    this.dbSubtitleRef.doc(subId).update({translation});
+                });
+            });
+
+            this.dbSubtitleRef
+                .onSnapshot(snapshot => {
+                    snapshot.docChanges.forEach(change => {
+                        if (change.type === "added") {
+                            this.addSubtitleToDom(change.doc.data(), change.doc.id);
+                        }
+                        if (change.type === "modified") {
+                            this.updateSubtitleInDom(change.doc.data(), change.doc.id);
+                        }
+                        if (change.type === "removed") {
+                            console.log("Removed: ", change.doc.data());
+                        }
+                    });
+                });
+        }
+
+        private getSubitleTemplate(sub: ISubtitle, id: string): JQuery {
+            const $template = $(`
+            <div class="sub" data-subid="${id}">
+                <p class="original">${sub.subtitle}</p>
+            </div>`);
+
+            if (sub.translation)
+                $template.append(`<p class="translation">${sub.translation}</p>`);
+
+            return $template;
+        }
+
+        private addSubtitleToDom(sub: ISubtitle, id: string) {
+            // todo: use an dictionary to keep reference of subtitles in DOM by id
+            const $template = this.getSubitleTemplate(sub, id);
+            this.$container.append($template);
+
+            // scroll to the bottom
+            this.$container[0].scrollTop = this.$container[0].scrollHeight;
+        }
+
+        private updateSubtitleInDom(sub: ISubtitle, id: string) {
+            const $template = this.getSubitleTemplate(sub, id);
+
+            // todo: use an dictionary to keep reference of subtitles in DOM by id
+            this.$container.find(`.sub[data-subid=${id}]`).replaceWith($template);
+        }
+
+        private deleteSubtitleFromDom() {
+
+        }
+
+    }
+}
+
+$(function () {
+    const network = new thdk.network.HttpNetwork();
+    const app = new thdk.rst.RemoteSubtitleReceiver(config, new thdk.translate.google.GoogleTranslate(config, network));
+    app.init();
+});
---
+++ src/js/lib.ts
@@ -0,0 +1,104 @@
+namespace thdk.rst {
+    export interface ISubtitle {
+        id: string;
+        subtitle: string;
+        translation?: string;
+    }
+}
+
+namespace thdk.translate {
+    export interface ITranslateService {
+        translate(text: string): Promise<string>;
+    }
+
+    export namespace google {
+        export interface IGoogleConfig {
+            apiKey: string;
+        }
+        export interface IGoogleTranslateConfig extends IGoogleConfig {
+        }
+
+        export class GoogleTranslate implements ITranslateService {
+            private apiKey: string;
+            private network: thdk.network.INetwork;
+
+            constructor(config: IGoogleTranslateConfig, network: thdk.network.INetwork) {
+                this.apiKey = config.apiKey;
+                this.network = network;
+            }
+
+            public translate(text): Promise<string> {
+                return this.translateText(text);
+            }
+
+            private translateText(text): Promise<string> {
+                var url = 'https://translation.googleapis.com/language/translate/v2?key=' + this.apiKey;
+                var data = {
+                    q: text,
+                    target: 'en',
+                    format: 'text',
+                    source: 'cs',
+                    model: 'nmt',
+                    key: this.apiKey
+                };
+                return this.network.postData<{data: { translations: {model: string, translatedText: string}[]}}>(url, data).then(response => {
+                    console.log(response);
+                    return response;
+                }).then(r => r.data.translations[0].translatedText);
+            };
+        }
+    }
+}
+
+namespace thdk.network {
+    export interface INetwork {
+        postData<T>(url: string, data: any): Promise<T>;
+    }
+
+    export class HttpNetwork implements INetwork {
+        private http = function (method, url, body, success, error) {
+            var xhr = new XMLHttpRequest();
+            xhr.open(method, url, true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState !== 4) { return; }
+                if (xhr.status >= 400) {
+                    // notify('API request failed');
+                    console.log('XHR failed', xhr.responseText);
+                    error(xhr.responseText);
+                    return;
+                }
+                success(JSON.parse(xhr.responseText));
+            };
+            xhr.send(body);
+        };
+
+        public postData<T>(url, data): Promise<T> {
+            return new Promise((resolve, reject) => {
+                this.http("POST", url, JSON.stringify(data), resolve, reject);
+            });
+        }
+    }
+
+    export class FetchNetwork implements INetwork {
+        public postData<T>(url, data): Promise<T> {
+            // Default options are marked with *
+            return fetch(url, {
+                body: JSON.stringify(data), // must match 'Content-Type' header
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                mode: 'cors', // no-cors, cors, *same-origin
+                redirect: 'follow', // manual, *follow, error
+                referrer: 'client', // *client, no-referrer
+            })
+            .then(response => {
+                if (!response.ok)
+                    throw new Error(response.statusText)
+
+                  return response.json()
+                });
+        }
+    }
+}
